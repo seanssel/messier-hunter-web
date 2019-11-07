15de8e9afd9215619c3aea438635c94c504d754c
seanssel <2467750+seanssel@users.noreply.github.com>
2019-11-07 00:39:48 -0800

    Precache index (working offline now)

--- scripts/swgen.js
+++ scripts/swgen.js
@@ -3,6 +3,11 @@ const workboxBuild = require("workbox-build");
 // Run after all assets are built
 workboxBuild.generateSW({
   globDirectory: "public",
-  globPatterns: ["**/*.{css,woff,ico,jpg,png,js,json}"],
-  swDest: "public/sw.js"
+  globPatterns: ["**/*.{html,css,woff,ico,jpg,png,js,json}"],
+  swDest: "public/sw.js",
+   templatedURLs: {
+    '/': [
+      '../src/views/index.hbs',
+     ],
+  }
 });
