b72edb4496a9a0b676508291d8706cbd4da957cf
seanssel <2467750+seanssel@users.noreply.github.com>
2019-11-07 16:04:07 -0800

    Get rid of NODE_ENV production check for purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -13,6 +13,6 @@ module.exports = {
   plugins: [
     require("tailwindcss"),
     require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+    [purgecss]
   ]
 };
